795cffbcdc0baf4b944278eb9ac6359f3d9ae1df
Oscar Oswaldo Meza Leon <oscar.meza.leon@gmail.com>
2020-02-20 18:54:20 -0700

    FAMILIA DE VISTAS EN NORHTWIND
    Se anadio el resto de restrcciones de las vistas y ademas se anadio las vistas que se usaran en los trabajos de northwind

--- unidad2vistas/RESTRICCIONESVISTAS.sql
+++ unidad2vistas/RESTRICCIONESVISTAS.sql
@@ -35,6 +35,78 @@ GO
 SELECT * FROM VW_PRODUCTOS6 ORDER BY PRODUCTNAME;
 
 --5. SI SE ELIMINAN OBJETOS A LOS QUE SE HACE REFERENCIA DENTRO DE UNA VISTA--
+--LA VISTA PERMANECE LA SIGUIENTE VEZ QUE INTENTE USAR ESA VISTA RECIBIRA UN MENSAJE DE ERROR--
 
+CREATE VIEW VW_PRODUCTOS7 AS 
+SELECT * FROM VW_PRODUCTOS6
+GO
+DROP VIEW VW_PRODUCTOS6
+--LA VISTA 7 YA NO SE EJECUTA, LA VSITA 6 FUE ELIMINDA PREVIAMENTE
+SElECT * FROM VW_PRODUCTOS7 ORDER BY PRODUCTID
+
+--6 NO PUEDE HACER REFERENCIA A TABLAS TEMPORALES EN UNA VISTA.
+--TAMPOCO PUEDE UTILIZAR LA CLAUSULA SELECT INTO DENTRO DE LA VISTA.
+
+--TABLA TEMPORAL LOCAL
+create table #local(col 1 int, col2 int)
+--tabla temporal global
+create table ##global(col 1 int, col2 int)
+
+select * from #local
+select * from ##global
+--marca error
+create view vw_productos5 as
+select * from #local
+
+--SELECT INTO, copia la estructura de una tabla y la llena de datos
+select *
+INTO #COPIAPROD
+from products
+
+select * from #copiaprod
+
+drop table #COPIAPROD
+
+
+--marca error
+create view vw_productos5 as
+select *
+into tabla4
+from products;
+
+
+-- 7 SI LA VISTA EMPLEA EL ASTERISCO * EN LA INSTRUCCION SELECT Y
+-- LA TABLA BASE A LA QUE HACE REFERENCIA SE LE AGREGAN NUEVAS COLUMNAS,
+--ESTAS NO SE MOSTRARAN EN LA VISTA
+
+create table tabla1(col1 int, col2 int)
+go
+create view vw_tablaA as
+select * from tabla1
+go
+alter table tabla 1 add col3 int
+go
+select * from vw_tablaA
+--Es necesario utilizar el comando alter view para actualizar los campos en la vista
+alter view vw_tablaA as
+select * from tabla1
+--al eliminar una columna de tabla1, la vista marcara error al ejecutarse
+alter table tabla1 drop column col1
+
+
+select * from vw_tablaA
+--se corrige ejecutando alter view
+
+--8 Si se crea una vista hija con base en una vista padre, debe tomar presente lo que esta haciendo la vista padre.
+
+--9 Los datos de las vistas no se almacenan por separado, si cambia un dato en una vista esta modificando
+-- el dato en la tabla base--
+update vw_products set unitprice = unitprice + 10 where productid = 1;
+
+select * from products where productid = 1;
+
+--10 En una vista no se puede hacer referencia a mas de 1024 columnas
+
+--11 En una vista no se puede crear indices ni desencadenadores ( triggers)
 
 
